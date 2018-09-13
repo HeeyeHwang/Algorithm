e694aaf315784453c1eaac8716864aacdc0622de
heeye <gdshy221@gmail.com>
2018-09-13 23:41:47 +0900

    programmers

--- HeeyeahNote/src/note/apple/september/Solution.java
+++ HeeyeahNote/src/note/apple/september/Solution.java
@@ -1,6 +1,8 @@
 package note.apple.september;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
@@ -8,15 +10,16 @@ public class Solution {
 	 * 완주하지 못한 선수 문제 설명 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을
 	 * 완주하였습니다.
 	 * 
-	 * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
-	 * 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+	 * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질
+	 * 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 	 * 
-	 * 제한사항 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다. completion의 길이는 participant의
-	 * 길이보다 1 작습니다. 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다. 참가자 중에는 동명이인이 있을 수
-	 * 있습니다. 입출력 예 participant completion return [leo, kiki, eden] [eden, kiki] leo
-	 * [marina, josipa, nikola, vinko, filipa] [josipa, filipa, marina, nikola]
-	 * vinko [mislav, stanko, mislav, ana] [stanko, ana, mislav] mislav 입출력 예 설명 예제
-	 * #1 leo는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+	 * 제한사항 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다. completion의 길이는
+	 * participant의 길이보다 1 작습니다. 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다. 참가자
+	 * 중에는 동명이인이 있을 수 있습니다. 입출력 예 participant completion return [leo, kiki,
+	 * eden] [eden, kiki] leo [marina, josipa, nikola, vinko, filipa] [josipa,
+	 * filipa, marina, nikola] vinko [mislav, stanko, mislav, ana] [stanko, ana,
+	 * mislav] mislav 입출력 예 설명 예제 #1 leo는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지
+	 * 못했습니다.
 	 * 
 	 * 예제 #2 vinko는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
 	 * 
@@ -26,47 +29,88 @@ public class Solution {
 	 * 
 	 * Solution.java
 	 * 
-	 * 1 class Solution { 2 public String solution(String[] participant, String[]
-	 * completion) { 3 String answer = ""; 4 return answer; 5 } 6 } 실행 결과 실행 결과가 여기에
-	 * 표시됩니다.
+	 * 1 class Solution { 2 public String solution(String[] participant,
+	 * String[] completion) { 3 String answer = ""; 4 return answer; 5 } 6 } 실행
+	 * 결과 실행 결과가 여기에 표시됩니다.
 	 */
 	public String solution(String[] participant, String[] completion) {
-		
-		String answer = null;
-		
-		return answer;
-	}
-	
-	public String solution_fail(String[] participant, String[] completion) {
+
 		String answer = null;
 
-		//System.out.println(Math.pow(2, 17)); = 131072.0
-		
+		for (int i = 0; i < completion.length; i++) {
+
+			for (int j = 0; j < participant.length; j++) {
+
+				if (completion[i] != null && completion[i].equals(participant[j])) {
+					participant[j] = null;
+					completion[i] = null;
+				}
+			}
+		}
+
+		for (int i = 0; i < participant.length; i++) {
+			if (participant[i] != null) {
+				answer = participant[i];
+			}
+		}
+
+		return answer;
+	}
+
+	public String solution_success(String[] participant, String[] completion) {
+		String answer = null;
+
+		// System.out.println(Math.pow(2, 17)); = 131072.0
+
 		Arrays.sort(participant);
 		Arrays.sort(completion);
-		
-		for(int i = 0 ; i < completion.length ; i++) {
+
+		System.out.println(Arrays.toString(participant));
+		System.out.println(Arrays.toString(completion));
+
+		int i;
+		for (i = 0; i < completion.length; i++) {
 
 			String partici = participant[i];
 			String comple = completion[i];
-			
-			if(!partici.equals(comple)) {
+
+			if (!partici.equals(comple)) {
 				answer = partici;
+				return answer;
+			}
+		}
+		answer = participant[i];
+		return answer;
+	}
+
+	public boolean solution(String[] phoneBook) {
+		
+		Arrays.sort(phoneBook);
+
+		System.out.println(Arrays.toString(phoneBook));
+		String stnd;
+		for(int i = 0 ; i < phoneBook.length ; i++) {
+			stnd = phoneBook[i];
+
+			for(int j = i+1 ; j < phoneBook.length ; j++) {
+				
+				if(phoneBook[j].startsWith(stnd)) {
+					return false;
+				}
 			}
 		}
 		
-		if(answer == null) {
-			answer = participant[participant.length -1];
-		}
-		
-		return answer;
+		return true;
 	}
 
 	public static void main(String[] args) {
 
-		String[] participant = null;
-		String[] completion = null;
+		String[] participant = new String[] { "mislav", "stanko", "mislav", "ana" };
+		String[] completion = new String[] { "stanko", "ana", "mislav" };
 
-		new Solution().solution(participant, completion);
+		String[] phoneBook = new String[] {"1235","123","12","567", "88"};
+		
+		boolean d = new Solution().solution(phoneBook);
+		System.out.println(d);
 	}
 }
