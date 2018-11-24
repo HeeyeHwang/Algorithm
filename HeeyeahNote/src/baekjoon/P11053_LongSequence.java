eda5f6718ca28b02047397c26829695b3a8902f3
heeyeah <gdshy221@gmail.com>
2018-11-24 18:35:43 +0900

    P11053 - dp, 최장수열 문제 (done)

--- HeeyeahNote/src/baekjoon/P11053_LongSequence.java
+++ HeeyeahNote/src/baekjoon/P11053_LongSequence.java
@@ -1,6 +1,9 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 
 public class P11053_LongSequence {
 
@@ -19,10 +22,49 @@ public class P11053_LongSequence {
 			seq[i] = sc.nextInt();
 		}
 
-		getLongestLength(n, seq);
+		getLongestLength2(n, seq);
 		sc.close();
 	}
+	
+	/**
+	 * dp[x] 는 x index의 값을 마지막으로 하는 수열의 최장 길이 값.
+	 * dp[x]를 어떻게 정의하냐에 따라 다른 것 같다.
+	 * @param n
+	 * @param seq
+	 */
+	public static void getLongestLength2(int n, int[] seq) {
+		
+		int[] dp = new int[n];
 
+		int max = 1;
+		dp[0] = 1;
+		for(int i = 1 ; i < n ; i++) {
+			dp[i] = 1;
+			
+			for(int j = 0 ; j < i ; j++) {
+			
+				// seq[i] > seq[j] 는 `증가`하는 부분 수열이니 당연한 조건이고
+				// dp[i] = Max ( dp[j] + 1, dp[i] ) 
+				// 이건, 이전 값의 best와 현재 증가하는 값 + 1을 비교했을 때, 가장 긴!
+				// ( j는 현재 current node라고 생각하고 , dp[i]는 j가 돌았던 이전의 값 중 max값 )
+				if(seq[i] > seq[j] ) { 
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+		
+			if(max < dp[i]) {
+				max = dp[i];
+			}
+		}
+		
+		System.out.println(max);
+	}
+
+	/**
+	 * 1st try. 2 1 2 와 같은 반례엔 답이 안나옴.
+	 * @param n
+	 * @param seq
+	 */
 	public static void getLongestLength(int n, int[] seq) {
 
 		int[] rslt = new int[n];
